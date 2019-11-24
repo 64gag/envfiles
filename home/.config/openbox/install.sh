82d23e0f760b083e7e991f869bcc54e5141497ae
Pedro Aguiar <paguiar32@gmail.com>
2019-11-24 21:15:47 +0100

    [add] Install script and better paths in lxde-rc.xml

---
+++ home/.config/openbox/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+dir_script=$(realpath $(dirname "$0"))
+file_name_target=~/.config/openbox/lxde-rc.xml
+
+cd "$dir_script"
+
+cp "$file_name_target" "$file_name_target.backup"
+sed -e '/<keyboard>/,/<\/keyboard>/!b' -e '/<\/keyboard>/!d;r lxde-rc.xml' -e 'd' -i "$file_name_target"
+
+openbox --reconfigure
