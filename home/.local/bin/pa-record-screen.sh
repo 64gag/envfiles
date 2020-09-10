93b766cd8a0f3d87c7475f0e90c4ae7c966f7514
Pedro Aguiar <pedro.aguiar@squadrone-system.com>
2020-09-10 14:50:23 +0200

    Take filename from argument

--- home/.local/bin/pa-record-screen.sh
+++ home/.local/bin/pa-record-screen.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+0,0 -c:v mpeg1video -q:v 3 -c:a libmp3lame output.avi
+ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+0,0 -c:v mpeg1video -q:v 3 -c:a libmp3lame "$1.avi"
+#ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i hw:0 -c:v mpeg1video -q:v 3 -c:a libmp3lame "$1.avi"
+
+# To record multiple screens...
+#ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+0,0 -c:v mpeg1video -q:v 3 -c:a libmp3lame "$1-top.avi" &
+#ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+0,1080 -c:v mpeg1video -q:v 3 -c:a libmp3lame "$1-bottom.avi"
