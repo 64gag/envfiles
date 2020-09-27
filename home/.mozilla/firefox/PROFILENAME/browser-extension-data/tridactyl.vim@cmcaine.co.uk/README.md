696a2a530de06663802de90e6764bf9a365cb751
Pedro Aguiar <paguiar@protonmail.com>
2020-09-27 16:57:16 +0200

    Delete tridactyl stuff (now using Vixen)

--- home/.mozilla/firefox/PROFILENAME/browser-extension-data/tridactyl.vim@cmcaine.co.uk/README.md
+++
@@ -1,3 +0,0 @@
-# Must run:
-set storageloc local
-# To make tridactyl read/write the storage.js file
--- home/.mozilla/firefox/PROFILENAME/browser-extension-data/tridactyl.vim@cmcaine.co.uk/storage.js
+++
@@ -1,1 +0,0 @@
-{"state":{"mode":"normal"},"userconfig":{"nmaps":{"l":"tabnext","h":"tabprev","gh":"back","gl":"forward","j":"scrollline 10","k":"scrollline -20"},"storageloc":"local"}}
