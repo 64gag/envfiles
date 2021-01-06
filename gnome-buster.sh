fbd23a20d0e2bd9c15871538999ce1a5ba1937db
Pedro Aguiar <paguiar@protonmail.com>
2021-01-06 18:58:54 +0100

    Rely on "Put windows" extension default shortcuts
    And other minor modifications?

--- gnome-buster.sh
+++ gnome-buster.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-gsettings set org.gnome.mutter dynamic-workspaces false
-gsettings set org.gnome.mutter workspaces-only-on-primary false
+gsettings reset-recursively org.gnome.desktop.wm.preferences
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
-gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.desktop.wm.preferences audible-bell false
+gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
+gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier "<Alt>"
+
+gsettings reset-recursively org.gnome.desktop.wm.keybindings
 gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Alt>F2', '<Super>r']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>h']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>j']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>k']"
@@ -16,68 +23,86 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Super><Sh
 gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"
 gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>i']"
 gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Super>u']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-side-w "['<Super>y']"
+#gsettings set org.gnome.desktop.wm.keybindings move-to-side-w "['<Super>y']"
 #gsettings set org.gnome.desktop.wm.keybindings move-to-side-s "['<Super>u']"
 #gsettings set org.gnome.desktop.wm.keybindings move-to-side-n "['<Super>i']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-side-e "['<Super>o']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-corner-nw "['<Super><Shift>y']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-corner-sw "['<Super><Shift>u']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-corner-se "['<Super><Shift>i']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-corner-ne "['<Super><Shift>o']"
+#gsettings set org.gnome.desktop.wm.keybindings move-to-side-e "['<Super>o']"
+#gsettings set org.gnome.desktop.wm.keybindings move-to-corner-nw "['<Super><Shift>y']"
+#gsettings set org.gnome.desktop.wm.keybindings move-to-corner-sw "['<Super><Shift>u']"
+#gsettings set org.gnome.desktop.wm.keybindings move-to-corner-se "['<Super><Shift>i']"
+#gsettings set org.gnome.desktop.wm.keybindings move-to-corner-ne "['<Super><Shift>o']"
 
-gsettings reset-recursively org.gnome.settings-daemon.plugins.media-keys
+gsettings reset-recursively org.gnome.desktop.interface
+gsettings set org.gnome.desktop.interface clock-show-date true
 
-#gsettings set org.gnome.settings-daemon.plugins.media-keys volume-mute "['XF86AudioMute', '<Super>m']"
-#gsettings set org.gnome.settings-daemon.plugins.media-keys volume-mute "['XF86AudioMute', '<Super>m']"
-#gsettings set org.gnome.settings-daemon.plugins.media-keys volume-mute 'XF86AudioMute'
-#gsettings get org.gnome.settings-daemon.plugins.media-keys volume-mute
+gsettings reset-recursively org.gnome.desktop.background
+gsettings set org.gnome.desktop.background show-desktop-icons true
 
-custom_keybindings_names=()
-custom_keybindings_command=()
-custom_keybindings_bindings=()
+gsettings reset-recursively org.gnome.desktop.input-sources
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+altgr-intl')]"
+
+gsettings reset-recursively org.gnome.mutter
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+
+gsettings reset-recursively org.gnome.shell
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-function custom_keybindings_add() {
-	local IFS=""
-	local name="$1"
-	local command="$2"
-	local binding="$3"
-	custom_keybindings_names=(${custom_keybindings_names[@]} "${name}")
-	custom_keybindings_commands=(${custom_keybindings_commands[@]} "${command}")
-	custom_keybindings_bindings=(${custom_keybindings_bindings[@]} "${binding}")
+function media_keys_custom_keybindings_reset() {
+    gsettings reset-recursively org.gnome.settings-daemon.plugins.media-keys
+
+    #gsettings set org.gnome.settings-daemon.plugins.media-keys volume-mute "['XF86AudioMute', '<Super>m']"
+    #gsettings set org.gnome.settings-daemon.plugins.media-keys volume-mute "['XF86AudioMute', '<Super>m']"
+    #gsettings set org.gnome.settings-daemon.plugins.media-keys volume-mute 'XF86AudioMute'
+    #gsettings get org.gnome.settings-daemon.plugins.media-keys volume-mute
+
+    custom_keybindings_names=()
+    custom_keybindings_command=()
+    custom_keybindings_bindings=()
 }
 
-function custom_keybindings_apply() {
-	local custom_keybindings_prefix='/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings'
-	local custom_keybindings_schemas=()
-
-	for i in "${!custom_keybindings_names[@]}"; do
-		name="${custom_keybindings_names[$i]}"
-		custom_keybindings_schemas=(${custom_keybindings_schemas[@]} "'${custom_keybindings_prefix}/custom${i}/'")
-	done
-	gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[$(join_by ',' "${custom_keybindings_schemas[@]}")]"
-
-	for i in "${!custom_keybindings_names[@]}"; do
-		name="${custom_keybindings_names[$i]}"
-		gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom_keybindings_prefix}/custom${i}/ name "${name}"
-	done
-
-	for i in "${!custom_keybindings_commands[@]}"; do
-		command="${custom_keybindings_commands[$i]}"
-		gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom_keybindings_prefix}/custom${i}/ command "${command}"
-	done
-
-	for i in "${!custom_keybindings_bindings[@]}"; do
-		binding="${custom_keybindings_bindings[$i]}"
-		gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom_keybindings_prefix}/custom${i}/ binding "${binding}"
-	done
+function media_keys_custom_keybindings_add() {
+    local IFS=""
+    local name="$1"
+    local command="$2"
+    local binding="$3"
+    custom_keybindings_names=(${custom_keybindings_names[@]} "${name}")
+    custom_keybindings_commands=(${custom_keybindings_commands[@]} "${command}")
+    custom_keybindings_bindings=(${custom_keybindings_bindings[@]} "${binding}")
 }
 
-custom_keybindings_add "terminal" "gnome-terminal" "'<Super>t'"
-custom_keybindings_add "calculator" "gnome-calculator" "'<Super>c'"
-custom_keybindings_add "nautilus" "nautilus -w" "'<Super>f'"
-custom_keybindings_add "pavucontrol" "pavucontrol" "'<Super>a'"
-custom_keybindings_add "firefox" "firefox" "'<Super>b'"
+function media_keys_custom_keybindings_apply() {
+    local custom_keybindings_prefix='/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings'
+    local custom_keybindings_schemas=()
 
-custom_keybindings_apply
+    for i in "${!custom_keybindings_names[@]}"; do
+        name="${custom_keybindings_names[$i]}"
+        custom_keybindings_schemas=(${custom_keybindings_schemas[@]} "'${custom_keybindings_prefix}/custom${i}/'")
+    done
+    gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[$(join_by ',' "${custom_keybindings_schemas[@]}")]"
+
+    for i in "${!custom_keybindings_names[@]}"; do
+        name="${custom_keybindings_names[$i]}"
+        gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom_keybindings_prefix}/custom${i}/ name "${name}"
+    done
+
+    for i in "${!custom_keybindings_commands[@]}"; do
+        command="${custom_keybindings_commands[$i]}"
+        gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom_keybindings_prefix}/custom${i}/ command "${command}"
+    done
+
+    for i in "${!custom_keybindings_bindings[@]}"; do
+        binding="${custom_keybindings_bindings[$i]}"
+        gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom_keybindings_prefix}/custom${i}/ binding "${binding}"
+    done
+}
+
+media_keys_custom_keybindings_reset
+media_keys_custom_keybindings_add "terminal" "gnome-terminal" "'<Super>t'"
+media_keys_custom_keybindings_add "calculator" "gnome-calculator" "'<Super>c'"
+media_keys_custom_keybindings_add "nautilus" "nautilus -w" "'<Super>f'"
+media_keys_custom_keybindings_add "pavucontrol" "pavucontrol" "'<Super>a'"
+media_keys_custom_keybindings_add "firefox" "firefox" "'<Super>b'"
+media_keys_custom_keybindings_apply
