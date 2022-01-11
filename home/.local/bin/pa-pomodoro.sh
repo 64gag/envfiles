185d496f5906bd0b7931ed17bec6e28070fef36f
Gael Aguiar <gael.aguiar@kymasoft.com>
2022-01-11 16:37:26 +0100

    pomodoro: record video and audio separately, merge them at the end

--- home/.local/bin/pa-pomodoro.sh
+++ home/.local/bin/pa-pomodoro.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+
 notify-send -t 1000 -u low "Starting"
 
-ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i hw:0 -c:v mpeg1video -q:v 3 -c:a libmp3lame "$1.avi" &
-ffmpeg_pid=$!
+name_basename_arg=$1
+name_date=$(date +"%Y-%m-%d_%H-%M-%S")
+name_basename="${name_basename_arg}-${name_date}"
+
+ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i hw:0 -c:v mpeg1video -q:v 3 -c:a libmp3lame "${name_basename}-video.avi" &
+#ffmpeg -video_size 1920x1080 -f x11grab -framerate 30 -i $DISPLAY -c:v h264_nvenc -qp 0 "${name_basename}-video.avi" &
+ffmpeg_video_pid=$!
+#ffmpeg -f pulse -i alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink.monitor "${name_basename}-audio.wav" &
+ffmpeg -f pulse -i alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source.echo-cancel "${name_basename}-audio.wav" &
+ffmpeg_audio_pid=$!
 
 sleep 10m
 notify-send -t 1000 -u low "15 minutes left"
@@ -15,5 +24,7 @@ notify-send -t 1000 -u normal "1 minute left"
 sleep 1m
 notify-send -t 1000 -u critical "Stopping"
 
-kill -s SIGINT $ffmpeg_pid
-wait
+trap "kill -s SIGINT $ffmpeg_audio_pid $ffmpeg_video_pid " SIGINT
+wait ${ffmpeg_video_pid}
+
+ffmpeg -i "${name_basename}-video.avi" -i "${name_basename}-audio.wav" -c copy "${name_basename}.avi"
