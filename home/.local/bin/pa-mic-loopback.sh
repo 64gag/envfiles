753ee3a1fa9b7e1710b75b538d2fc45a1197941b
Gael Aguiar <gael.aguiar@kymasoft.com>
2022-01-11 16:38:34 +0100

    More arguments/parameters (more control, less intuitive)

--- home/.local/bin/pa-mic-loopback.sh
+++ home/.local/bin/pa-mic-loopback.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
-if [ "$1" = "off" ]; then
-  pactl unload-module module-loopback
-else
-  pactl load-module module-loopback latency_msec=1
-fi
+
+while [[ $# -gt 0 ]]
+do
+    case $1 in
+        --echo-cancel)
+            pactl load-module module-echo-cancel use_volume_sharing=no
+            shift
+            ;;
+        --loopback)
+            pactl load-module module-loopback latency_msec=10
+            shift
+            ;;
+        --unload)
+            shift
+            pactl unload-module module-loopback
+            pactl unload-module module-echo-cancel
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
