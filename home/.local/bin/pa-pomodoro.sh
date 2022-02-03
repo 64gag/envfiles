290723b6999fc8bd08a5ef7556f87772dfb9b790
Gael Aguiar <gael.aguiar@kymasoft.com>
2022-02-03 15:32:41 +0100

    Modify to match home setup (the only relevant setup...? ;)
    - Record webcam
    - Use GPU H264 encoding

--- home/.local/bin/pa-pomodoro.sh
+++ home/.local/bin/pa-pomodoro.sh
@@ -1,30 +1,55 @@
 #!/usr/bin/env bash
 
-notify-send -t 1000 -u low "Starting"
+sigint_received=0
+notify-send -t 2500 -u low "Starting"
 
 name_basename_arg=$1
 name_date=$(date +"%Y-%m-%d_%H-%M-%S")
 name_basename="${name_basename_arg}-${name_date}"
 
-ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i hw:0 -c:v mpeg1video -q:v 3 -c:a libmp3lame "${name_basename}-video.avi" &
-#ffmpeg -video_size 1920x1080 -f x11grab -framerate 30 -i $DISPLAY -c:v h264_nvenc -qp 0 "${name_basename}-video.avi" &
-ffmpeg_video_pid=$!
-#ffmpeg -f pulse -i alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink.monitor "${name_basename}-audio.wav" &
-ffmpeg -f pulse -i alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source.echo-cancel "${name_basename}-audio.wav" &
+ffmpeg -f pulse -i alsa_input.usb-046d_HD_Pro_Webcam_C920_6B753E4F-02.analog-stereo.echo-cancel "${name_basename}-audio.wav" &
 ffmpeg_audio_pid=$!
 
-sleep 10m
-notify-send -t 1000 -u low "15 minutes left"
-sleep 5m
-notify-send -t 1000 -u normal "10 minutes left"
-sleep 5m
-notify-send -t 1000 -u normal "5 minutes left"
-sleep 4m
-notify-send -t 1000 -u normal "1 minute left"
-sleep 1m
-notify-send -t 1000 -u critical "Stopping"
+ffmpeg -vaapi_device /dev/dri/renderD128 -f x11grab -video_size 1920x1080 -i :0 -vf 'hwupload,scale_vaapi=format=nv12' -c:v h264_vaapi -qp 24 "${name_basename}-screen.avi" &
+ffmpeg_screen_pid=$!
 
-trap "kill -s SIGINT $ffmpeg_audio_pid $ffmpeg_video_pid " SIGINT
-wait ${ffmpeg_video_pid}
+ffmpeg -vaapi_device /dev/dri/renderD128 -f v4l2 -video_size 640x480 -i /dev/video0 -vf 'format=nv12,hwupload' -c:v h264_vaapi "${name_basename}-cam.avi" &
+ffmpeg_cam_pid=$!
 
-ffmpeg -i "${name_basename}-video.avi" -i "${name_basename}-audio.wav" -c copy "${name_basename}.avi"
+
+function run_cleanup()
+{
+    kill $sleep_pid
+    kill -s SIGQUIT $ffmpeg_cam_pid $ffmpeg_screen_pid $ffmpeg_audio_pid
+    sigint_received=1
+}
+
+trap run_cleanup SIGINT
+
+notification_intervals_m=(10 5 5 4 1)
+remaining_time_m=0
+
+for notification_interval_m in "${notification_intervals_m[@]}"
+do
+    remaining_time_m=$((remaining_time_m + $notification_interval_m))
+done
+
+for notification_interval_m in "${notification_intervals_m[@]}"
+do
+    sleep "${notification_interval_m}m"
+    sleep_pid=$!
+    if [[ "$sigint_received" -eq 1 ]]; then
+        break
+    fi
+    remaining_time_m=$((remaining_time_m - notification_interval_m))
+    if [[ "$remaining_time_m" -gt 0 ]]; then
+        notify-send -t 2500 -u low "${remaining_time_m} minutes left"
+    fi
+done
+
+notify-send -t 2500 -u critical "Stopping"
+run_cleanup
+
+wait ${ffmpeg_audio_pid} ${ffmpeg_screen_pid} ${ffmpeg_cam_pid}
+
+ffmpeg -i "${name_basename}-screen.avi" -i "${name_basename}-audio.wav" -c copy "${name_basename}-screen-and-audio.avi"
