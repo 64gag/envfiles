af6834a2aa1eafd60525b5de7efcb9200008940e
Gael Aguiar <gael.aguiar@kymasoft.com>
2023-08-30 15:28:45 +0200

    Add duplicity support, encryption key prompt and minor improvements

--- home/.local/bin/64gag-backup.doc.sh
+++ home/.local/bin/64gag-backup.doc.sh
@@ -7,10 +7,10 @@ backup_dir="/tmp"
 exclude_values=(/bin /sbin /boot /dev /proc /sys /run /media /mnt /usr /lib* /snap /var/cache /var/lib /var/log)
 exclude_values+=(/home/*/.gvfs /home/*/.cache /home/*/.local/share/Trash /lost+found)
 exclude_values+=(/tmp)
-exclude_values+=(tmp*)
 
 date=$(date "+%F-%H-%M-%S")
 description="64gag-backup"
+backup_name="${HOSTNAME}-${description}"
 
 # NOTE: all the parameters up to this point can be modified in the following sourced file
 
@@ -19,35 +19,63 @@ if [ -f "${conf_file}" ]; then
     source "${conf_file}"
 fi
 
-exclude_values+=(${backup_dir}) # Exclude the final value of this variable
+if [ -z "${PASSPHRASE+x}" ]; then # duplicity uses PASSPHRASE so we just play along
+    echo -n "Enter encryption key: "
+    read -s PASSPHRASE
+    echo
+    echo -n "Confirm encryption key: "
+    read -s PASSPHRASE_CONFIRM
+    echo
 
-backup_tar_file="${backup_dir}/${HOSTNAME}-${date}-${description}.tar.gz"
-backup_tar_checksum_file="${backup_tar_file}.sha256"
-backup_tar_encrypted_file="${backup_tar_file}.gpg"
-backup_tar_encrypted_checksum_file="${backup_tar_encrypted_file}.sha256"
+    if [ "$PASSPHRASE" != "$PASSPHRASE_CONFIRM" ]; then
+        echo "Encryption keys do not match. Aborting."
+        exit 1
+    fi
+
+    export PASSPHRASE
+fi
+
+exclude_values+=(${backup_dir}) # Exclude the final value of this variable
 
 exclude_arguments=()
 for item in "${exclude_values[@]}"; do
-    exclude_arguments+=("--exclude=$item")
+    exclude_arguments+=("--exclude")
+    exclude_arguments+=("$item")
 done
 
 # Actual action begins
 mkdir -p "${backup_dir}"
-time tar -cvpzf "${backup_tar_file}" "${exclude_arguments[@]}" /
-time sha256sum "${backup_tar_file}" > ${backup_tar_checksum_file}
 
-echo ""
-echo ""
-echo "Backup file created: ${backup_tar_file}"
-du -sh "${backup_tar_file}"
-echo "sha256 checksum found in: ${backup_tar_checksum_file}"
-cat "${backup_tar_checksum_file}"
+if [[ "${backup_tool}" == "duplicity" ]]; then
+    exclude_arguments+=("--exclude")
+    exclude_arguments+=(**/tmp*) # TODO confirm this works
 
-echo "Encrypting the document with gpg to: ${backup_tar_encrypted_file}"
-gpg --output "${backup_tar_encrypted_file}" --symmetric "${backup_tar_file}"
-time sha256sum "${backup_tar_encrypted_file}" > ${backup_tar_encrypted_checksum_file}
+    duplicity --verbosity Info --archive-dir "${backup_dir}" --name "${backup_name}" "${exclude_arguments[@]}" / "${duplicity_target_url}"
+else
+    exclude_arguments+=("--exclude")
+    exclude_arguments+=(tmp*) # duplicity does not like this syntax...
 
-echo "sha256 checksum found in: ${backup_tar_encrypted_checksum_file}"
-cat "${backup_tar_encrypted_checksum_file}"
+    backup_tar_file="${backup_dir}/${backup_name}-${date}.tar.gz"
+    backup_tar_checksum_file="${backup_tar_file}.sha256"
+    backup_tar_encrypted_file="${backup_tar_file}.gpg"
+    backup_tar_encrypted_checksum_file="${backup_tar_encrypted_file}.sha256"
 
-split -b 50G "${backup_tar_encrypted_file}" "${backup_tar_encrypted_file}_part_"
+    time tar -cvpzf "${backup_tar_file}" "${exclude_arguments[@]}" /
+    time sha256sum "${backup_tar_file}" > ${backup_tar_checksum_file}
+
+    echo ""
+    echo ""
+    echo "Backup file created: ${backup_tar_file}"
+    du -sh "${backup_tar_file}"
+    echo "sha256 checksum found in: ${backup_tar_checksum_file}"
+    cat "${backup_tar_checksum_file}"
+
+    echo "Encrypting the document with gpg to: ${backup_tar_encrypted_file}"
+    gpg --batch --passphrase $PASSPHRASE --output "${backup_tar_encrypted_file}" --symmetric "${backup_tar_file}"
+    time sha256sum "${backup_tar_encrypted_file}" > ${backup_tar_encrypted_checksum_file}
+
+    echo "sha256 checksum found in: ${backup_tar_encrypted_checksum_file}"
+    cat "${backup_tar_encrypted_checksum_file}"
+
+    split -b 200M "${backup_tar_encrypted_file}" "${backup_tar_encrypted_file}_part_"
+fi
