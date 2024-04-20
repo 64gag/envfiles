bf0519a95364490922099eac9c785823a1f746a3
Gael Aguiar <gael.aguiar@kymasoft.com>
2024-04-20 11:06:41 +0200

    Changes during Steam VM installation

---
+++ home/.local/bin/gag-fedora-install-multimedia-plugins.doc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# https://docs.fedoraproject.org/en-US/quick-docs/installing-plugins-for-playing-movies-and-music/
+# https://docs.fedoraproject.org/en-US/quick-docs/rpmfusion-setup/
+
+sudo dnf -y update
+sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y group update core
+sudo dnf -y group install --allowerasing Multimedia
---
+++ home/.local/bin/gag-fedora-install-steam.doc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo dnf install -y fedora-workstation-repositories
+sudo dnf install -y steam --enablerepo=rpmfusion-nonfree-steam
