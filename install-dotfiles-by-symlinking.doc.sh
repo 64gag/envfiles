5f7783995f8be27588bfff058201c617e7359391
Gael Aguiar <gael.aguiar@kymasoft.com>
2025-05-06 17:12:01 +0200

    VS Code settings

--- install-dotfiles-by-symlinking.doc.sh
+++ install-dotfiles-by-symlinking.doc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# TODO Make sure this script is executed from a given location
+# TODO GAG This script is ugly AF
+# TODO GAG Make sure this script is executed from a given location
 
 loop_dir=home
 for i in ${loop_dir}/.*; do
@@ -8,3 +9,6 @@ for i in ${loop_dir}/.*; do
         ln -s "$(pwd)/$i" ~
     fi
 done
+
+mkdir -p $HOME/.config/Code/User
+ln -s $PWD/home/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json
